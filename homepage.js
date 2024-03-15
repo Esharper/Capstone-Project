311c0058be7acfd45da4472303b01684399a95aa
Esharper <elginsharper87@gmail.com>
2024-03-15 04:01:32 +0000

    add

---
+++ homepage.js
@@ -0,0 +1,7 @@
+// Custom JavaScript
+// Add your custom JavaScript functionality here
+
+// Example: Change text on button click
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+  document.querySelector('h1').textContent = 'Text Changed!';
+});
